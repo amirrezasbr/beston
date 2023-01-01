0910b2ae8f57973571404d5f407b795637177852
amirrezasbr <amirreza13.7676@gmail.com>
2023-01-01 18:14:47 +0330

    add some income

--- web/admin.py
+++ web/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Exopense
+from .models import Exopense, Income
 # Register your models here.
 
 
 admin.site.register(Exopense)
+admin.site.register(Income)
--- web/models.py
+++ web/models.py
@@ -1,10 +1,26 @@
+from __future__ import unicode_literals
+from django.utils.translation import gettext_lazy as _
+
 from django.db import models
 from django.contrib.auth.models import User
 # Create your models here.
 
 
 class Exopense(models.Model):
+    text = models.CharField(_("Name"), max_length=255)
+    date = models.DateTimeField()
+    amount = models.BigIntegerField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    def __str__(self):
+        return self.text, self.amount
+
+
+class Income(models.Model):
     text = models.CharField(max_length=255)
     date = models.DateTimeField()
     amount = models.BigIntegerField()
     user = models.ForeignKey(User, on_delete=models.CASCADE)
+    def __str__(self):
+        return self.text, self.amount
+#class Meta:
+ #       verbose_name = _('text')
